c825896855b3fdd1ec45bd12a45aeba9592c1461
Dylan Barber <DylanBarber1@Outlook.com>
2019-10-17 15:25:38 -0400

    Add post route to add a new user

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 require('dotenv').config()
 const express = require("express");
 const MongoClient = require("mongodb").MongoClient;
+const bodyParser = require("body-parser");
 const ObjectId = require('mongodb').ObjectId;
 
 const app = express();
+
+app.use(bodyParser.json());
+
 if (process.env.NODE_ENV === "production") {
   uri = process.env.ATLAS_URI;
 } else {
@@ -26,16 +30,17 @@ MongoClient.connect(uri, {
     db = client.db(dbName)
   });
 
-  app.get('/', (req, res) => {
-    const collection = db.collection('notes')
-    collection.find({}).toArray((err, data) => {
-      if (err) return res.status(500).send(err); 
-      res.send(data);
-    })
-    // res.send('test received');
-  })
 
-  const port = process.env.PORT || 25565; 
-  app.listen(port, (req, res) => {
-    console.log(`Listening on ${port}`);
+app.post('/add', (req, res) => {
+  const collection = db.collection('notes')
+  if (req.body === undefined) return res.status(400).send("No insert parameters were supplied");
+  collection.insertOne(req.body, (err, data) => {
+    if (err) return res.status(500).send(err);
+    res.send(data);
   })
+})
+
+const port = process.env.PORT || 25565;
+app.listen(port, (req, res) => {
+  console.log(`Listening on ${port}`);
+})
