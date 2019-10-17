fb672dc78df76e02fb1b743ac727b456c5a624db
Dylan Barber <DylanBarber1@Outlook.com>
2019-10-17 15:34:34 -0400

    Add eslint fixes

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
 const MongoClient = require("mongodb").MongoClient;
 const bodyParser = require("body-parser");
-const ObjectId = require('mongodb').ObjectId;
+const ObjectId = require("mongodb").ObjectId;
 
 const app = express();
 
@@ -12,35 +12,43 @@ if (process.env.NODE_ENV === "production") {
   uri = process.env.ATLAS_URI;
 } else {
   // localhost
-  uri = process.env.LOCAL_URI
+  uri = process.env.LOCAL_URI;
 }
 
-let db = ""
-let dbName = "notetaker"
+let db = "";
+let dbName = "notetaker";
 MongoClient.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 },
   (err, client) => {
     if (err) {
-      console.log(err)
-      return
+      console.log(err);
+      return;
     }
-    console.log("Connected successfully to server")
-    db = client.db(dbName)
+    console.log("Connected successfully to server");
+    db = client.db(dbName);
   });
 
 
-app.post('/add', (req, res) => {
-  const collection = db.collection('notes')
+app.post("/add", (req, res) => {
+  const collection = db.collection("notes");
   if (req.body === undefined) return res.status(400).send("No insert parameters were supplied");
   collection.insertOne(req.body, (err, data) => {
     if (err) return res.status(500).send(err);
     res.send(data);
-  })
-})
+  });
+});
+
+app.get("/", (req, res) => {
+  const collection = db.collection("notes");
+  collection.find({}, (err, data) => {
+    if (err) return res.status(500).send(err);
+    res.send(data);
+  });
+});
 
 const port = process.env.PORT || 25565;
 app.listen(port, (req, res) => {
   console.log(`Listening on ${port}`);
-})
+});
